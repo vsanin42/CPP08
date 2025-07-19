bafb0473a2827abdd6e6180f8ea55e779ba4f516
Vladislav Sanin <vladislav.sanin.419@gmail.com>
2025-07-19 19:58:25 +0200

    ex02 done

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vsanin <vsanin@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:57:53 by vsanin            #+#    #+#             */
-/*   Updated: 2025/07/19 17:18:08 by vsanin           ###   ########.fr       */
+/*   Updated: 2025/07/19 19:41:11 by vsanin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,9 @@
 #include <list>
 #include <deque>
 #include <vector>
+
+static void printMutant(MutantStack<int>& stack);
+static void constPrintMutant(const MutantStack<int>& stack);
 
 int main(void)
 {
@@ -139,21 +142,112 @@ int main(void)
 		std::stack<int, std::vector<int> > s(mstack);
 	}
 	{
-		std::cout << "\n~~~ big MutantStack - wrong ~~~" << std::endl;
+		std::cout << "\n~~~ MutantStack, printing with different iterator types ~~~" << std::endl;
 		MutantStack<int> mstack;
-		for (int i = 0; i < 200; i++)
+		for (int i = 0; i < 42; i++)
 			mstack.push(i);
-		std::cout << mstack.size() << std::endl;
-		
-		MutantStack<int>::iterator it = mstack.begin();
-		MutantStack<int>::iterator ite = mstack.end();
-		
+		std::cout << "size: " << mstack.size() << std::endl;
+		printMutant(mstack);
+		std::cout << std::endl;
+		constPrintMutant(mstack);
+		std::cout << std::endl;
+	}
+	{
+		std::cout << "\n~~~ empty MutantStack ~~~" << std::endl;
+		MutantStack<int> empty;
+		std::cout << "is MutantStack empty? " << ((empty.begin() == empty.end()) ? "yes" : "no") << std::endl;
+	}
+	{
+		std::cout << "\n~~~ ocf ~~~" << std::endl;
+		MutantStack<char> a = MutantStack<char>();
+		std::cout << "size a (default constructor): " << a.size() << std::endl;
+		a.push('a');
+		a.push('b');
+		a.push('c');
+		std::cout << "size a (after push): " << a.size() << std::endl;
+		std::cout << "a:";
+		MutantStack<char>::iterator it = a.begin();
+		MutantStack<char>::iterator ite = a.end();
 		while (it != ite)
 		{
-			std::cout << *it << " ";
+			std::cout << " " << *it;
+			++it;
+		}
+		std::cout << std::endl;
+
+		MutantStack<char> b(a);
+		std::cout << "size b (copy constructor): " << b.size() << std::endl;
+		std::cout << "b:";
+		it = b.begin();
+		ite = b.end();
+		while (it != ite)
+		{
+			std::cout << " " << *it;
+			++it;
+		}
+		std::cout << std::endl;
+
+		MutantStack<char> c;
+		std::cout << "size c (default constructor): " << c.size() << std::endl;
+		c = b;
+		std::cout << "size c (copy operator): " << c.size() << std::endl;
+		std::cout << "c:";
+		it = c.begin();
+		ite = c.end();
+		while (it != ite)
+		{
+			std::cout << " " << *it;
 			++it;
 		}
 		std::cout << std::endl;
 	}
 	return 0;
+}
+
+static void printMutant(MutantStack<int>& stack)
+{
+	MutantStack<int>::iterator it = stack.begin();
+	MutantStack<int>::iterator itend = stack.end();
+	
+	std::cout << "iterators:" << std::endl;
+	while (it != itend)
+	{
+		std::cout << *it << " ";
+		++it;
+	}
+	std::cout << std::endl;
+	
+	MutantStack<int>::reverse_iterator rit = stack.rbegin();
+	MutantStack<int>::reverse_iterator ritend = stack.rend();
+	
+	std::cout << "reverse iterators:" << std::endl;
+	while (rit != ritend)
+	{
+		std::cout << *rit << " ";
+		++rit;
+	}
+}
+
+static void constPrintMutant(const MutantStack<int>& stack)
+{
+	MutantStack<int>::const_iterator cit = stack.begin();
+	MutantStack<int>::const_iterator citend = stack.end();
+	
+	std::cout << "const iterators:" << std::endl;
+	while (cit != citend)
+	{
+		std::cout << *cit << " ";
+		++cit;
+	}
+	std::cout << std::endl;
+	
+	MutantStack<int>::const_reverse_iterator crit = stack.rbegin();
+	MutantStack<int>::const_reverse_iterator critend = stack.rend();
+	
+	std::cout << "const reverse iterators:" << std::endl;
+	while (crit != critend)
+	{
+		std::cout << *crit << " ";
+		++crit;
+	}
 }
